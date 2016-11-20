9bdb761f74c9a5ae971a3d7c0e4e470ac81bfdbe
Daniel Romero <dromero736@gmail.com>
2016-11-19 21:11:59 -0500

    PopUp Gui Updated, Suggestion in progress

--- src/edu/buffalo/cse116/code/PopUp.java
+++ src/edu/buffalo/cse116/code/PopUp.java
@@ -1,25 +1,38 @@
 package edu.buffalo.cse116.code;
 
+import com.sun.deploy.util.ArrayUtil;
+
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 /**
  * Created by dromsoft on 11/18/2016.
  */
-public class PopUp {
+public class PopUp{
 
     private JFrame _window;
     private JPanel _popupGui, _headerPanel, _bodyPanel, _suspectPanel, _weaponPanel, _roomPanel, _submitPanel;
     private JButton _suggestionButton, _accusationButton;
+    private User _chosenSuspect, _currentPlayer;
+    private String _suspectChosen, _weaponChosen, _roomChosen;
+    private ArrayList<User> _current;
+
+    public String get_suspectChosen(){ return _suspectChosen; }
+    public String get_weaponChosen(){ return _weaponChosen; }
+    public String get_roomChosen(){ return _roomChosen; }
+
     public JFrame get_window() {
         return _window;
     }
 
-    public PopUp() {
+    public PopUp(ArrayList<User> currentAList, User currentPlayer) {
+        _currentPlayer = currentPlayer;
+        _current = currentAList;
         _window = new JFrame();
         _window.setVisible(true);
         _window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -52,7 +65,8 @@ public class PopUp {
                 suspectTitle = BorderFactory.createTitledBorder(blackline, "Suspect");
                 suspectTitle.setTitleJustification(TitledBorder.CENTER);
                 _suspectPanel.setBorder(suspectTitle);
-                JComboBox<String> suspectNames = new JComboBox<String>(User.CHARACTER_NAME);
+                JComboBox<String> suspectNames = new JComboBox<String>(currentList(_current));
+                    _suspectChosen = String.valueOf(suspectNames.getSelectedItem());
                 _suspectPanel.add(suspectNames);
             _bodyPanel.add(_weaponPanel);
                 TitledBorder weaponTitle;
@@ -60,6 +74,7 @@ public class PopUp {
                 weaponTitle.setTitleJustification(TitledBorder.CENTER);
                 _weaponPanel.setBorder(weaponTitle);
                 JComboBox<String> weaponNames = new JComboBox<String>(Board.WEAPONS);
+                    _weaponChosen = String.valueOf(weaponNames.getSelectedItem());
                 _weaponPanel.add(weaponNames);
             _bodyPanel.add(_roomPanel);
                 TitledBorder roomTitle;
@@ -72,6 +87,12 @@ public class PopUp {
                 _roomPanel.add(currentRoom);
             _bodyPanel.add(_submitPanel);
                 JButton submitButton = new JButton("GO");
+                submitButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        suggestion();
+                    }
+                });
                 _submitPanel.add(submitButton);
         _window.add(_popupGui);
     }
@@ -97,7 +118,8 @@ public class PopUp {
                 suspectTitle = BorderFactory.createTitledBorder(blackline, "Suspect");
                 suspectTitle.setTitleJustification(TitledBorder.CENTER);
                 _suspectPanel.setBorder(suspectTitle);
-                JComboBox<String> suspectNames = new JComboBox<String>(User.CHARACTER_NAME);
+                JComboBox<String> suspectNames = new JComboBox<String>(currentList(_current));
+                    _suspectChosen = String.valueOf(suspectNames.getSelectedItem());
                 _suspectPanel.add(suspectNames);
             _bodyPanel.add(_weaponPanel);
                 TitledBorder weaponTitle;
@@ -105,6 +127,7 @@ public class PopUp {
                 weaponTitle.setTitleJustification(TitledBorder.CENTER);
                 _weaponPanel.setBorder(weaponTitle);
                 JComboBox<String> weaponNames = new JComboBox<String>(Board.WEAPONS);
+                    _weaponChosen = String.valueOf(weaponNames.getSelectedItem());
                 _weaponPanel.add(weaponNames);
             _bodyPanel.add(_roomPanel);
                 TitledBorder roomTitle;
@@ -112,9 +135,16 @@ public class PopUp {
                 roomTitle.setTitleJustification(TitledBorder.CENTER);
                 _roomPanel.setBorder(roomTitle);
                 JComboBox<String> roomNames = new JComboBox<String>(Board.ROOMS);
+                    _roomChosen = String.valueOf(roomNames.getSelectedItem());
                 _roomPanel.add(roomNames);
             _bodyPanel.add(_submitPanel);
                 JButton submitButton = new JButton("GO");
+                submitButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        accusation();
+                    }
+                });
                 _submitPanel.add(submitButton);
         _window.add(_popupGui);
     }
@@ -150,4 +180,60 @@ public class PopUp {
                 _window.pack();
             }});
     }
+
+    public void suggestion() {
+        _chosenSuspect = returnUser(_suspectChosen);
+        moveUserHere(_chosenSuspect);
+
+
+    }
+
+    /**
+     * Returns Chosen User from Array list of User based off their name
+     * @param chosenUserName
+     * @return User
+     */
+    public User returnUser(String chosenUserName) {
+        int index = 0;
+        for (int i = 0; i < User.CHARACTER_NAME.length; i++) {
+            if (User.CHARACTER_NAME[i] == chosenUserName) {
+                index = i;
+                break;
+            }
+        }
+        User chosen = _current.get(index);
+        return chosen;
+    }
+
+    /**
+     *
+     * @param user
+     */
+    public void moveUserHere(User user) {
+
+    }
+
+    /**
+     * Gets
+     */
+    public void accusation() {
+
+    }
+
+    /**
+     * Displays a array of names of current players in the game
+     * @param currentPlayers
+     * @return array of names of current players
+     */
+    public String[] currentList(ArrayList<User> currentPlayers) {
+        String[] retVal = new String[currentPlayers.size()];
+        for (int j = 0; j < currentPlayers.size(); j++) {
+            retVal[j] = User.CHARACTER_NAME[j];
+        }
+        return retVal;
+    }
+
+
+
+
 }
--- src/edu/buffalo/cse116/code/User.java
+++ src/edu/buffalo/cse116/code/User.java
@@ -12,18 +12,8 @@ public class User {
     private int _userTurn;
     private int _posX, _posY;
     private Board _board;
-    //  stores users' current coordinates {int x, int y}
-    private int[] _currentCoor;
-    //  stores users coordinates at beginning of turn {int x, int y}, for comparison
-    private int[] _startTurn;
-    //  stores users coordinates at end of turn {int x, int y}
-    private int[] _endTurn;
-    // holds value if user decides to end turn, prompts: continue: yes or no (OPTIONAL)
-    private boolean _decision;
- // Holds value to set off suggestion
-    private boolean _trigger;
-    // Hold value if the user wants to make a suggestion under certain circumstances
-    private boolean _makeSuggestion;
+    private int[] _startTurnCoor, _endTurnCoor, _currentCoor; // stores coordinates {int x, int y}
+    private boolean _trigger;// Holds value to set off suggestion
 
     /**
      * Creates a User on the game board and assigns it a characterName from the CHARACTER_NAME Array.
@@ -140,8 +130,8 @@ public class User {
      * @return int[] _startTurn;
      */
     public int[] startTurnCoordinates() {
-    	_startTurn = get_userCoor();
-    	return _startTurn;
+    	_startTurnCoor = get_userCoor();
+    	return _startTurnCoor;
     }
     
     /**
@@ -149,8 +139,8 @@ public class User {
      * @return int[] _endTurn;
      */
     public int[] endTurnCoordinates() {
-    	_endTurn = get_userCoor();
-    	return _endTurn;
+    	_endTurnCoor = get_userCoor();
+    	return _endTurnCoor;
     }
     /**
      * At the start of every turn call this method.
@@ -160,80 +150,11 @@ public class User {
      */
     public boolean userMoved() {
     	// if coordinates are then same then false,
-    	if ( _endTurn == _startTurn) {
+    	if ( _endTurnCoor == _startTurnCoor) {
     		return false;
     	} else {
     		// user moved to a new room! Call suggestion
     		return true;
-    	}
-    }
-    
-    /**
-     * This method returns boolean if the user coordinates {x,y} is equal to the doors coordinates {x,y},
-     *  which means that the user is entering a room.
-     *  This will be used to trigger a the suggestion method if true.
-     * @param  userCoor
-     * @return _trigger if true, user at door coordinates, false if otherwise
-     */
-    public boolean enterDoor(int[] userCoor) {
-    	int x = 0;
-    	int y = 1;
-    	boolean xBool = false;
-    	boolean yBool = false;
-    	
-    	// Iterates through every array in 2Darray
-    	for(int[] coor : Board.DOORCOOR) {
-    			// checks if first value (x) in the array matches
-    			if (coor[x] == userCoor[x]) {
-    				xBool = true;
-    			} 
-    			// checks if second value (y) in the array matches
-    			if (coor[y] == userCoor[y]) {
-    				yBool = true;
-    			}
-    	}
-    	// returns true if coordinates match
-    	_trigger = (xBool && yBool);
-    	return _trigger;
-    }
-    
-
-    /**
-     * if user entered new room, fire suggestion method
-     * if ended up in new room, fire option for suggestion
-     */
-    public void suggestion() {
-    	// if entered through door
-    	if (_trigger == true) {
-    		/**
-    		 * Prompts user for:
-    		 * What User to move? Excluding yourself
-    		 * User u1 or u2 etc..
-    		 * What weapon to move?
-    		 * Object weapon [1-6]
-    		 * this.location (current room)
-    		 */
-    		
-    		// if user was moved before start of turn, provide option to make suggestion
-    	} else if (userMoved()) {
-    		
-    		/**
-    		 * Prompt user to make a suggestion or not
-    		 * _makeSuggestion = yes or no
-    		 */
-    		// if yes
-    		if (_makeSuggestion == true) {
-    			/**
-        		 * Prompts user for:
-        		 * What User to move?
-        		 * What weapon to move?
-        		 * this.location
-        		 */
-    		} else {
-    			// check if can leave room
-    			// if you can continue with turn
-    			// otherwise: cry/end turn
-    		}
     	}
     }
     
@@ -253,18 +174,6 @@ public class User {
     	}
     	
     	return roomName;
-    }
-    
-    //TODO Finish method to move otheruser because of suggestion
-    /**
-     * Moves a user to current Room because of suggestion 
-     * @param otherUser
-     */
-    public void moveUserToRoom(User otherUser) {
-    	// Get current users location
-    	// Get otherUsers coordinates
-    	// Get room location
-    	// Change otherUsers location to room location
     }
     
 
@@ -318,6 +227,11 @@ public class User {
                 _board.useRoll();
                 _board.getGui().updateInfoPanel();
                 _board.getGui().updateBoard();
+
+                _trigger = true;
+
+                PopUp popUp = new PopUp(_board.getListOfPlayers(), _board.getCurrentPlayer());
+
                 return true;
             }
             //room -> room && room -> door
