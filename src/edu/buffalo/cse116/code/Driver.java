50ac26e7e47b7276a7b32f88a7ca65624e5ef546
Liam Gensel <liamgens@gmail.com>
2016-11-21 12:06:56 -0500

    Added the Driver class :)

--- src/edu/buffalo/cse116/code/Driver.java
+++ src/edu/buffalo/cse116/code/Driver.java
@@ -1,22 +1,15 @@
-package edu.buffalo.cse116.tests;
-
-import edu.buffalo.cse116.code.Board;
-import edu.buffalo.cse116.code.Card;
-import edu.buffalo.cse116.code.Gui;
+package edu.buffalo.cse116.code;
 
 import java.util.Scanner;
 
-import org.junit.Test;
-
-
 /**
- * Created by liamgens on 10/24/16.
+ * Created by liamgens on 11/21/16.
  */
-public class GuiTest {
+public class Driver {
     public static void main(String[]args){
 
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter number of players (3-6; there is no error handling here): ");
+        System.out.print("Enter number of players (3-6): ");
         int numberOfPlayers = scan.nextInt();
 
         Gui gui = new Gui(numberOfPlayers);
@@ -27,5 +20,4 @@ public class GuiTest {
         }
 
     }
-
 }
